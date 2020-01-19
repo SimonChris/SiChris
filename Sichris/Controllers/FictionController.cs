54306a33278987d700f0a0c3a6ea1a5314c3ed01
Simon Christiansen <simonchris1729@gmail.com>
2020-01-19 22:29:39 +0100

    Updates

---
+++ Sichris/Controllers/FictionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sichris.Controllers
+{
+  public class FictionController : Controller
+  {
+    //
+    // GET: /Fiction/
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
